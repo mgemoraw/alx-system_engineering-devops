4c6c5160427697b2490a8060f10f5c983944a385
mgemoraw <meng.get4@gmail.com>
2023-12-23 06:41:09 -0500

    my puppet manifest file to index.html

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,41 +1,21 @@
-# puppet manifest code automate works
-
-exec { 'dist update':
-  command => '/usr/bin/apt-get update',
-  provider => 'shell'
-}
-
-
-package { 'nginx': 
-  ensure => 'installed',
-  require => Exec['dist update']
+# My puppet manifest file
+package { 'nginx':
+    ensure => installed,
 }
 
 file_line { 'install':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-enabled/default',
-  after   => 'listen 80  default_server;',
-  line    => 'rewrite ^/redirect_me https://github.com/mgemoraw permanent;',
+  ensure    => 'present',
+  path      => '/etc/nginx/sites-enabled/default',
+  after     => 'listen 80 default-server;',
+  line      => 'rewrite ^/redirect_me https://www.github.com/mgemoraw/ permanent;',
 }
 
 # creating index file
 file { '/var/www/html/index.html':
-  content => "Hello World!",
+  content   => "Hello World!",
 }
 
-# creating error page file
-file {'/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-}
-
-# restart service
-exec { 'restart service':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
-
-# start service nginx
 service { 'nginx':
-  ensure => running,
-  require => Package['nginx'],
+  ensure    => running,
+  require   => Package['nginx']
 }
